d5d7c593efdbc33be022a19c2a65bcbabb93b018
KEVIN STIFEL <krstifel@uc.cl>
2024-04-03 17:49:06 -0300

    Change API_BASE_URL

--- src/helpers/apiService.js
+++ src/helpers/apiService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://backend-integram.onrender.com/';
 
 // Get all users or filter them by name
 const getUsers = async (name = '') => {
